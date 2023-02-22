2207397213b956acab855d0c0c1d440248abac4e
Alejandro Huertas <ahuertas@fi.uba.ar>
2023-02-22 20:04:06 -0300

    confi proyecto

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import express from 'express';
+import { config } from './utils/config.js';
 const app = express();
 
-let PORT = 8080
+let PORT = config.node.port || 8080
 
 
 app.get('/', (req, res) => {
     res.send('Hola Git')
+})
+
+app.get('/ruta-dev', (req, res) => {
+    res.send('Hola dev')
 })
 
 app.listen(PORT, () => {
